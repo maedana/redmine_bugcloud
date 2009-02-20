d3f80f44adebd558cb7eaed5a9682fda62427f37
maedana <maeda.na@gmail.com>
2009-02-20 16:59:54 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_bugcloud do
+  name 'Redmine Bugcloud plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
