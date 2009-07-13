3fa96eaa9307fae496476622c368ee2e9bee31a3
maedana <maeda.na@gmail.com>
2009-07-13 20:45:03 +0900

    Patch for Rails 2.2.x I18N and making it a project-module written by MischaTheEvil

--- init.rb
+++ init.rb
@@ -4,8 +4,13 @@ Redmine::Plugin.register :redmine_bugcloud do
   name 'Redmine Bugcloud plugin'
   author 'maedana'
   description 'This is a plugin for Redmine. The view that displays the ticket like the tag cloud is added. '
-  version '0.0.1'
+  version '0.0.2'
 
-  permission :bugcloud, { :bugcloud => [:index] }, :public => true
+  # Configuring permissions for plugin's controllers.
+  project_module :bugcloud do
+    permission :view_bugcloud, { :bugcloud => [:index] }, :public => true
+  end
+ 
+  # Creating menu entry. It appears in project menu as the last entry.
   menu :project_menu, :bugcloud, { :controller => 'bugcloud', :action => 'index' }, :caption => :bugcloud, :last => true
 end
